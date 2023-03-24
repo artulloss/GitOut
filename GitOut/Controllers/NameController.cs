16a322ceec763f903fbca0c94dfd2918cc10b860
artulloss <35859316+artulloss@users.noreply.github.com>
2023-03-24 19:36:34 -0400

    Name controller

---
+++ GitOut/Controllers/NameController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace GitOut.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return JsonSerializer.Serialize(new
+            {
+                name = "Adam Tulloss"
+            });
+        }
+    }
+}
